ab65af686a600d34e8f8bb53c32981244e82e89d
Matheus Moreira <matheusmoreira08@gmail.com>
2022-06-20 02:08:31 -0300

    conexão do Client e Server enviando dados do game com Socket.io

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import http from "http";
 import createGame from "./public/game.js";
-import {Server} from "socket.io";
+import { Server } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
@@ -13,13 +13,17 @@ const game = createGame();
 
 game.addPlayer({ playerId: "player1", playerX: 0, playerY: 0 });
 game.addPlayer({ playerId: "player2", playerX: 9, playerY: 9 });
+game.addPlayer({ playerId: "player3", playerX: 9, playerY: 0 });
 game.addFruit({ fruitId: "fruit1", fruitX: 3, fruitY: 3 });
+game.addFruit({ fruitId: "fruit2", fruitX: 3, fruitY: 6 });
 
 console.log(game.state);
 
 sockets.on("connection", (socket) => {
   const playerId = socket.id;
   console.log(`> Player connected on Server with id: ${playerId}`);
+
+  socket.emit("setup", game.state);
 });
 
 server.listen(3000, () => {
